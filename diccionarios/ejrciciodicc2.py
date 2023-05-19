f8fdf5e3b5ede8520c6e894740d0dc1b091c6f8e
brayanmata27 <brayanbenavides2211@gmail.com>
2023-05-19 10:10:16 -0500

    ejercicio diccionario

---
+++ diccionarios/ejrciciodicc2.py
@@ -0,0 +1,42 @@
+diccionarioe={"gato": "cat" , 
+            "perro": "dog" , 
+            "abeja": "bee" , 
+            "conejo": "rabbit" , 
+            "gallina": "hen" , 
+            "vaca": "cow" , 
+            "serpiente": "snake" , 
+            "cerdo": "pig",
+            "pajaro": "bird",
+            "araña": "spider"}
+
+words=[input('ingrese animal del diccionario en español')]
+for word in words:
+    if word in diccionarioe:
+        print(word, "-->", diccionarioe[word])
+    else:
+        print(word, 'no esta en el diccionario')
+
+
+diccionarioi={"cat": "gato" , 
+            "dog": "perro" , 
+            "bee": "abeja" , 
+            "rabbit": "conejo" , 
+            "hen": "gallina" , 
+            "cow": "vaca" , 
+            "snake": "serpiente" , 
+            "pig": "cerdo",
+            "bird": "pajaro",
+            "spider": "araña"}
+
+words=[input('ingrese animal del diccionario en ingles')]
+for word in words:
+    if word in diccionarioi:
+        print(word, "-->", diccionarioi[word])
+    else:
+        print(word, 'no esta en el diccionario')
+
+def seleccion1 (opcion1):
+    opcion1=='I'
+    palabraI=[words]
+    for palabraI in palabraI:
+
