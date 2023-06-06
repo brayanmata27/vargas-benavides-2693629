22ce6811dc71ee80e7aa319f41b6bee2849868ab
brayanmata27 <brayanbenavides2211@gmail.com>
2023-06-06 16:11:10 -0500

    tarea empleado

---
+++ POO/empleado.py
@@ -0,0 +1,35 @@
+class empleado:
+
+    objetos=0
+def __init__(self,nombre,cargo,salario):
+    self.nombre=nombre
+    self.cargo=cargo
+    self.salario=salario
+    empleado.objetos+=1
+
+    def getnombre(self):
+        return self.nombre
+    
+    def setnombre(self,nombre):
+        self.nombre=nombre
+
+    def getcargo(self):
+        return self.crago
+    
+    def setcargo(self,cargo):
+        self.cargo=cargo
+
+    def getsalrio(self):
+        return salario
+    
+    def setsalario(self,salario):
+        self.salario=salario
+
+    def calcularHora(self):
+        horastrabajo=8
+        diassemana=5
+        diasmes=20
+        salariohora=self.salario /(horastrabajo * diassemana * diasmes)
+        return self.__salariohora
+
+    
---
+++ POO/empleadoapp.py
@@ -0,0 +1,6 @@
+from empleado import *
+
+empleado1 = empleado("Jose", "dearrollador", 1160)
+
+#print("empleado1 salario hora", empleado1.calcularHora.salariohora())
+print(type(empleado1))
