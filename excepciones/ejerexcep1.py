cc86b05c072c8cdb7c97b125dbb147fc94e34fb3
brayanmata27 <brayanbenavides2211@gmail.com>
2023-07-18 21:33:43 -0500

    formula c

---
+++ excepciones/ejerexcep1.py
@@ -0,0 +1,8 @@
+#formula cuadratica
+def Formula_cuadratica():
+    try:
+        a=float(input("Digite valor para a"))
+        b=float(input("Digite valor para b"))
+        c=float(input("Digite valor para c"))
+        x=b**2 -(4*a*c)
+    
